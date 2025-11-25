bfbcddaced1518e5e7aa968f2fce514fee91f758
Mohammed Ammar Al Hamadani <mohammedamaar165@gmail.com>
2025-11-25 21:44:18 +0100

    sollwerte currents blocks simu

--- build_stromregelung.m
+++ build_stromregelung.m
@@ -17,3 +17,17 @@ add_block('simulink/Sources/In1', [modelName '/IW_meas']);  %       W
 set_param([modelName '/IU_meas'], 'Position', [50, 50, 80, 70]);
 set_param([modelName '/IV_meas'], 'Position', [50, 100, 80, 120]);
 set_param([modelName '/IW_meas'], 'Position', [50, 150, 80, 170]);
+
+
+% sollstrom Vorgabe und ON/OFF .. Konstanten
+add_block('simulink/Sources/Constant', [modelName '/IU_SOLL']);  % Sollwert für Phase U
+set_param([modelName '/IU_SOLL'], 'Value', '1');  % 1 Ampere 
+set_param([modelName '/IU_SOLL'], 'Position', [50, 250, 80, 270]);
+
+add_block('simulink/Sources/Constant', [modelName '/IV_SOLL']);  %  V
+set_param([modelName '/IV_SOLL'], 'Value', '-0.5');  % Negativer für symmetrische Drehstrom
+set_param([modelName '/IV_SOLL'], 'Position', [50, 300, 80, 320]);
+
+add_block('simulink/Sources/Constant', [modelName '/IW_SOLL']);  %  W
+set_param([modelName '/IW_SOLL'], 'Value', '-0.5');  % auch negativ
+set_param([modelName '/IW_SOLL'], 'Position', [50, 350, 80, 370]);
